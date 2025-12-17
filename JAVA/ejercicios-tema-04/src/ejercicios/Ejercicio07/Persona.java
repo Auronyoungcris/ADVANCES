425951d5b4ee723db74a7325e6d76f1498789eaa
Daniel Vazquez Chinchilla <danivxinxill@gmail.com>
2025-12-17 18:40:18 +0100

    Add files via upload

---
+++ JAVA/ejercicios-tema-04/src/ejercicios/Ejercicio07/Persona.java
@@ -0,0 +1,18 @@
+package ejercicios.Ejercicio07;
+
+public class Persona {
+	private String nombre;
+	private Integer edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+}
