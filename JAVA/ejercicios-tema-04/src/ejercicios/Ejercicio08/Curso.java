f0d9d0940414cf7971c5ca4508c783ee5d659070
Daniel Vazquez Chinchilla <danivxinxill@gmail.com>
2025-12-17 18:41:26 +0100

    Add files via upload

---
+++ JAVA/ejercicios-tema-04/src/ejercicios/Ejercicio08/Curso.java
@@ -0,0 +1,22 @@
+package ejercicios.Ejercicio08;
+
+public class Curso {
+	private Integer identificador;
+	private String descripcion;
+
+	public Integer getidentificador() {
+		return identificador;
+	}
+
+	public void setidentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
